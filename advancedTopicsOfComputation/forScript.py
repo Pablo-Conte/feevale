c4b6cfedf59085a1cdc60eaa9825bf28eb7dc29a
PABLO CONTE CORRÊA <0414249@feevale.br>
2024-08-20 22:00:18 -0300

    feat: the rest of the code

---
+++ advancedTopicsOfComputation/forScript.py
@@ -0,0 +1,5 @@
+def main():
+    for number in range(10):
+        print(number)
+
+main()
---
+++ advancedTopicsOfComputation/whileScript.py
@@ -0,0 +1,12 @@
+def main():
+    number = 5
+    
+    while number < 12:
+        print(number)
+        number = number + 1
+
+    print("Exit")
+
+main()
+
+        
